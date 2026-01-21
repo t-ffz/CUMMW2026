d01afb732947be5700c876bb987fe07cccc8f3a3
Pooja Prabakaran <pooja@Poojas-MacBook-Air.local>
2026-01-20 23:57:27 -0500

    standardized values

--- normdata.py
+++ normdata.py
@@ -3,3 +3,8 @@ import pandas as pd
 df = pd.read_csv('t2data.csv')
 
 print(df)
+
+col_names = df.select_dtypes(include=['float64', 'int64']).columns
+df[col_names] = (df[col_names] - df[col_names].mean())/df[col_names].std()
+
+print(df) 
